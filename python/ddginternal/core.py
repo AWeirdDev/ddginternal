8c80373565e588e9f2eb8f380c52880989b79c11
`` <amengpan6@gmail.com>
2024-08-21 11:19:55 +0800

    parms

--- python/ddginternal/core.py
+++ python/ddginternal/core.py
@@ -15,7 +15,10 @@ from .primp import Client
 from .utils import raise_for_status
 
 ModuleNames = Literal["places", "recipes"]
-
+user_agent = (
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) "
+    "Chrome/125.0.0.0 Safari/537.36 OPR/111.0.0.0"
+)
 
 def organic_search(q: str, **kwargs) -> Dict[Literal["html", "djs"], str]:
     """Gets the contents of `d.js` and the HTML.
@@ -36,13 +39,16 @@ def organic_search(q: str, **kwargs) -> Dict[Literal["html", "djs"], str]:
     client = Client(impersonate="chrome_127", verify=False)
 
     # Get the start page (simpler search, possibly fewer ads)
-    res = client.get("https://start.duckduckgo.com", params={"q": q})
+    payload = {"q": q}.update(kwargs)
+    if not payload:
+        payload = {"q": q}
+    res = client.get("https://start.duckduckgo.com", params=payload)
     raise_for_status(res)
 
     # The /d.js file stores all information about the search results
     djs_url = get_djs(res.text)
 
-    djs_res = client.get("https://links.duckduckgo.com" + djs_url, params=kwargs)
+    djs_res = client.get("https://links.duckduckgo.com" + djs_url)
     raise_for_status(djs_res)
 
     return {"html": res.text, "djs": djs_res.text}
